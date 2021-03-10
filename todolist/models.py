810790903edf32cc52c60d9925b2b87c3f471222
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-10 16:25:52 +0600

    Create models and migrate

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class Todo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    notes = models.TextField(blank=True)
