0ded1807bd1e6e7caba4e59336a432e19f0e688a
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-03-24 17:19:08 +0600

    Update category api

--- web/todolist/serializers.py
+++ web/todolist/serializers.py
@@ -4,9 +4,18 @@ from .models import Category, Todo
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    chain = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
         fields = "__all__"
+
+    def get_chain(self, obj):
+        chain = []
+        while obj:
+            chain.append((obj.name, obj.id))
+            obj = obj.parent
+        return chain
 
 
 class TodoSerializer(serializers.ModelSerializer):
